667debddaaf0bfbde8695b76c5780bad035c1134
Nguyễn Huỳnh Minh Khuê <nguyenhuynhminhkhue@MacBook-Pro-cua-Nguyen-2.local>
2023-06-04 08:40:25 +0700

    first-040623

--- earthday-login/earthday-login.js
+++ earthday-login/earthday-login.js
@@ -1,43 +1,73 @@
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-auth.js";
+import { app } from "../firebase.js";
+import { showToast } from "../utils/showToast.js"
+const auth = getAuth();
+
 const userList = JSON.parse(localStorage.getItem('userList')) || []
 
 const form = document.getElementById('form')
 const email = document.getElementById('email')
 const password = document.getElementById('password')
-const message = document.getElementById('message')
 
 form.onsubmit = function (event) {
     event.preventDefault()
 
-    const loginUser = {
-        email: email.value,
-        password: password.value,
-    }
-
-    let flag = false
-
-    // Biến 2 object thành 2 chuỗi rồi so sánh
-    for (let user of userList) {
-        if ((loginUser.email == user.email) && (loginUser.password == user.password)) {
-            flag = true
-            break
-        }
-        else if (loginUser.email != user.email) {
-            message.textContent = 'This account does not exist.'
-        }
-        else if (loginUser.password != user.password) {
-            message.textContent = 'Please enter the valid password.'
-            console.log(loginUser.password, user.password)
-        }
-    }
-
-    if (flag) {
-        // User ton tai (email va password chinh xac)
-        localStorage.setItem('user', JSON.stringify(loginUser))
-
-        email.value = ''
-        password.value = ''
-
-        // Chuyen den trang dang nhap
-        window.location.href = '../index.html'
-    }
+    signInWithEmailAndPassword(auth, email.value, password.value)
+        .then((userCredential) => {
+            // Signed in 
+            const user = userCredential.user;
+            showToast('Successfully signed in')
+            // ...
+            window.location.href = '../'
+        })
+        .catch((error) => {
+            const errorMessage = error.message;
+            showToast(errorMessage, 'error')
+        });
 }
+
+
+
+// const userList = JSON.parse(localStorage.getItem('userList')) || []
+
+// const form = document.getElementById('form')
+// const email = document.getElementById('email')
+// const password = document.getElementById('password')
+// const message = document.getElementById('message')
+
+// form.onsubmit = function (event) {
+//     event.preventDefault()
+
+//     const loginUser = {
+//         email: email.value,
+//         password: password.value,
+//     }
+
+//     let flag = false
+
+//     // Biến 2 object thành 2 chuỗi rồi so sánh
+//     for (let user of userList) {
+//         if ((loginUser.email == user.email) && (loginUser.password == user.password)) {
+//             flag = true
+//             break
+//         }
+//         else if (loginUser.email != user.email) {
+//             message.textContent = 'This account does not exist.'
+//         }
+//         else if (loginUser.password != user.password) {
+//             message.textContent = 'Please enter the valid password.'
+//             console.log(loginUser.password, user.password)
+//         }
+//     }
+
+//     if (flag) {
+//         // User ton tai (email va password chinh xac)
+//         localStorage.setItem('user', JSON.stringify(loginUser))
+
+//         email.value = ''
+//         password.value = ''
+
+//         // Chuyen den trang dang nhap
+//         window.location.href = '../index.html'
+//     }
+// }
---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAM1FF_ZxCZX0571O8JZe6FIvNQnmFqvsY",
+    authDomain: "mindx-jsi02-app.firebaseapp.com",
+    projectId: "mindx-jsi02-app",
+    storageBucket: "mindx-jsi02-app.appspot.com",
+    messagingSenderId: "1021957968940",
+    appId: "1:1021957968940:web:0d90ad48c8218def892d89"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
